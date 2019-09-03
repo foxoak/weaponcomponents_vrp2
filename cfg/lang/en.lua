cd139466ccfc1d291cfd79266d75e0eae2e5b75f
foxoak <37819331+foxoak@users.noreply.github.com>
2019-09-03 15:04:32 -0300

    Fisrt Commit
    Weapon and Tint Component

---
+++ cfg/lang/en.lua
@@ -0,0 +1,56 @@
+
+-- INSERT INTO YOUR LANG FILE
+-- FoxOak
+
+---------------------
+-- THIS ALREADY EXIST
+--------------------
+  weapon = { -- THIS ALREADY EXIST
+    -- weapon translation by GTA 5 weapon name (lower case)
+    pistol = "Pistola"
+  },
+ -- ADD THIS 
+  component = {
+    -- weapon translation by GTA 5 weapon name 
+    AT_PI_SUPP = "Silenciador Pistola",
+    AT_AR_FLSH = "Lanterna Rifles",
+    AT_PI_FLSH = "Lanterna Pistolas"
+  },  
+  tint = {
+    green = "Verde",
+    gold = "Dourada",
+    pink = "Rosa",
+    army = "Exército",
+    lspd = "Policial",
+    orange = "Laranja",
+    platinum = "Cromada"
+  },  
+--------------------------
+  -- ITEM ALREADY EXIST
+-----------------------
+  item = {
+  	    wbody = {
+      name = "{1} arma",
+      description = "Arma {1}.",
+      equip = {
+        title = "Equipar"
+      }
+    },
+ ---------------
+ -------ADD THIS AFTER
+----------------
+
+    wcomp = {
+      name = "{1} Component",
+      description = "Component {1}.",
+      equip = {
+        title = "Equip"
+      }
+    },    
+    wtint = {
+      name = "Tint {1}",
+      description = "Color {1}.",
+      equip = {
+        title = "Paint"
+      }
+    },
